653772b07e1d52688b86a99393a4ed07b4d1463e
KokoFibo <kokonacci@gmail.com>
2026-02-02 09:11:20 +0700

    benerin tanggal hari libur1

--- app/Livewire/Test.php
+++ app/Livewire/Test.php
@@ -68,19 +68,12 @@ class Test extends Component
     $month = 11;
     $year = 2026;
 
+    dd('aman');
     $data = Yfrekappresensi::where('date', '2026-01-24')
       ->whereNot('late', null)
       ->get();
     // dd($data->count());
-    $cx = 0;
-    foreach ($data as $d) {
-      // dd($d);
-      $d->late = null;
-      $d->late_history = null;
-      $d->save();
-      $cx++;
-    }
-    dd('done', $cx);
+
 
 
 
