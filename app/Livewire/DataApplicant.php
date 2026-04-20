16a8bec55308679ca56cd7a0f3ff7e7bb1bcc6e6
KokoFibo <kokonacci@gmail.com>
2026-04-20 08:49:11 +0700

    create user

--- app/Livewire/DataApplicant.php
+++ app/Livewire/DataApplicant.php
@@ -195,6 +195,9 @@ class DataApplicant extends Component
                 'password' => Hash::make($dataApplicant->password),
             ]);
 
+            //Create USer
+            createUser($id_karyawan_terbaru);
+
 
             // hapus data applicant
             $dataApplicant->delete();
