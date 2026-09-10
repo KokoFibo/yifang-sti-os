d949d2146b7dfb5b8894b65c7fb732d51bf0963e
KokoFibo <kokonacci@gmail.com>
2026-09-10 11:32:21 +0700

    kasih role untuk update resigned

--- resources/views/livewire/updatekaryawanwr.blade.php
+++ resources/views/livewire/updatekaryawanwr.blade.php
@@ -108,9 +108,9 @@
                 </div>
 
                 <div class="d-flex flex-wrap gap-2 pb-3 px-3 justify-content-center" wire:loading.remove>
-                    @if (($status_karyawan != 'Resigned' && $status_karyawan != 'Blacklist') || auth()->user()->role >= 6)
-                        <button wire:click="update1" class="btn btn-primary">{{ __('Update') }}</button>
-                    @endif
+                    {{-- @if (($status_karyawan != 'Resigned' && $status_karyawan != 'Blacklist') || auth()->user()->role >= 6) --}}
+                    <button wire:click="update1" class="btn btn-primary">{{ __('Update') }}</button>
+                    {{-- @endif --}}
 
                     <button wire:click="exit" class="btn btn-dark">{{ __('Exit') }}</button>
 
